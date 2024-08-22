e51c08306c9e0b23601fde68680eb1063876ff91
王玉松 <wangyusong.vendor@sensetime.com>
2024-08-22 14:26:08 +0800

    Initial Commit

---
+++ OCDocumentDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OCDocumentDemo
+//
+//  Created by 王玉松 on 2024/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
