afd5e785d2f00d1c671de94f7d78449e98b3d93f
王玉松 <wangyusong.vendor@sensetime.com>
2024-10-24 11:44:03 +0800

    first commit

---
+++ OCDocumentDemo/SLDocumentViewController.h
@@ -0,0 +1,16 @@
+//
+//  SLDocumentViewController.h
+//  OCDocumentDemo
+//
+//  Created by 王玉松 on 2024/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLDocumentViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCDocumentDemo/SLRemoteFileViewController.m
@@ -0,0 +1,31 @@
+//
+//  SLRemoteFileViewController.m
+//  OCDocumentDemo
+//
+//  Created by 王玉松 on 2024/8/22.
+//
+
+#import "SLRemoteFileViewController.h"
+
+@interface SLRemoteFileViewController ()
+
+@end
+
+@implementation SLRemoteFileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
